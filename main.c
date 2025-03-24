e51cd8f4cfa928c2d674f5b068df172ee1010982
MaticSM1 <matic.s.musil@gmail.com>
2025-03-24 12:43:13 +0100

    Dodana funkcija in main

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
 
-int a = 1;
+#define TEST "Nek test pac"
+
+#ifdef __x86_64__
+    #define ARHITURA "64"
+#else
+    #define ARHITURA "32"
+#endif
+
+void test() {
+    printf("dober dan svet, kako kaj\n %s \n%s\n", TEST, ARHITURA);
+}
+
+int main() {
+    test();
+    return 0;
+}
